b10951f8d65cdbce900936a6e5fbbfa7b5e0eb53
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-04-27 21:26:05 -0300

    Resolvi o exercicio 3 de escrita de codigo, atraves de calculos e prompt

--- semana3/logica-booleana/index-escrita.js
+++ semana3/logica-booleana/index-escrita.js
@@ -1,28 +1,3 @@
-/*
-// Exercicio 2 - escrita de codigo//
-
-const nome = prompt("Qual é seu nome?")
-console.log("1.Qual é seu nome?")
-console.log("Resposta:", nome)
-
-const idade = prompt("Qual é sua idade?")
-console.log("2.Qual é sua idade?")
-console.log("Resposta:", idade)
-
-var altura = prompt("Qual a sua altura?")
-console.log("3.Qual a sua altura?")
-console.log("Resposta:", altura)
-
-var cidade = prompt("Onde você mora?")
-console.log("4.Onde você mora?")
-console.log("Resposta:", cidade)
-
-const signo = prompt("Qual o seu signo?")
-console.log("5.Qual o seu signo?")
-console.log("Resposta:", signo)
-
-*/
-
 
 // Exercicio 1 - escrita de codigo//
 
@@ -72,3 +47,63 @@ var resultadoLivreK = (f - 32)*5/9 + 273.15
 
 console.log(resultadoLivreF, "ºF")
 console.log(resultadoLivreK, "k")
+
+
+// Exercicio 2 - escrita de codigo//
+
+const nome = prompt("Qual é seu nome?")
+console.log("1.Qual é seu nome?")
+console.log("Resposta:", nome)
+
+const idade = prompt("Qual é sua idade?")
+console.log("2.Qual é sua idade?")
+console.log("Resposta:", idade)
+
+var altura = prompt("Qual a sua altura?")
+console.log("3.Qual a sua altura?")
+console.log("Resposta:", altura)
+
+var cidade = prompt("Onde você mora?")
+console.log("4.Onde você mora?")
+console.log("Resposta:", cidade)
+
+const signo = prompt("Qual o seu signo?")
+console.log("5.Qual o seu signo?")
+console.log("Resposta:", signo)
+
+
+
+// Exercicio 3 - escrita de codigo//
+
+/*
+//prompt para colocar qualquer valor gasto de enrgia//
+var quantidade = prompt("Energia gasta em quilowatt-hora?")
+var kwatth = 0.05
+var calculo = (quantidade*kwatth)
+console.log("R$", calculo, "de energia gasta")
+*/
+
+//a - gastando 280Kwatt-h// 
+
+var quantidade280 = 280
+var calculo280 = (quantidade280*kwatth)
+console.log(calculo280,"Kwatt-hora")
+
+
+
+//b//
+
+/*
+//prompt para colocar qualquer valor de desconto//
+
+var desconto = prompt("Porcentagem de desconto?")
+var porcentagemDecimal = 1-(desconto/100)
+var valorPromo = calculo*0.85
+console.log("R$", valorPromo, "de energia gasta com desconto")
+*/
+
+//desconto de 15%// 
+var valorPromo15 = calculo*0.85
+console.log("R$", valorPromo15, "de energia gasta com desconto")
+
+
