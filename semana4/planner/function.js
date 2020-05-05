01472dd2417ebc9ac420a617b6a899b78de18b16
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-05-05 16:22:00 -0300

    adicionei arquivos html, css, js

---
+++ semana4/planner/function.js
@@ -0,0 +1,20 @@
+//let diasDaSemana = document.getElementsByClassName()
+
+
+let tarefaNova //vai vir de dentro do input
+listaTarefas.innerHTML += <li>`${tarefaNova}`</li>
+
+
+function adicionarTarefa() {
+    let tarefa = document.getElementById('inputNovaTarefa')
+    let diaEscolhido = document.getElementById('seletorDiaDaSemana')
+
+} criarTarefa
+
+//pegar valor do options
+
+//direcionar os valores do options para sua div específica
+
+//copiar texto do input
+
+//direcionar texto para a div correta
