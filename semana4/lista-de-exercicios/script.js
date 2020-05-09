a5d321923dd50b3208255a285cdeca2c2ddd4709
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-05-09 16:50:44 -0300

    adicionei exercício 2 de interpretação

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -15,3 +15,32 @@ convertida em reais, esse valor será multiplicado
 pelo valor informado da cotação do dólar.
 */
 
+/*
+2. Foi criada uma função para calcular o valor
+final de um investimento, multiplicando o valor
+investido pela taxa de rendimento. 
+Criou-se uma função chamada investe dinheiro
+em que recebeu dois parâmetros de tipo de investimento
+e o seu valor. Criou-se uma variável sem valor
+inicial, a qual vai variar conforme o investimento
+escolhido. Foi usado o switch case, em que mostra opções
+fixas (cases) para cada investimento. No caso,
+foi informado na variável novo montante que o
+tipo de investimento escolhido foi "ações", portanto
+a função vai ir testando os cases até encontrar
+um case chamado "ações", quando encontrar vai 
+calcular o valor fornecido na const novo montante
+multiplicado pelo rendimento que é de 1.1. Como
+tem o break a função para de testar e coloca
+o valor obtido após o investimento (nesse caso vai
+ser de 165) no return. Com o console log, esse
+valor vai aparecer no console. O mesmo
+ocorrendo com o segundo montante, em que
+serão testado no switch até ser encontrado
+o case "tesouro direto", no caso, esse case
+não vai ser encontrado, portanto se encaixará
+no default, o que vai gerar uma mensagem de alerta
+escrita que "o tipo de investimento informado
+é incorreto".
+*/
+/*
