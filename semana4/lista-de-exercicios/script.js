946b03e8d2f2d2292160b1c0234d3bcccf8b242d
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-05-09 16:49:39 -0300

    adicionei exercício de interpretação 1

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,17 @@
+//Exercícios de interpretação de código:
+/*
+1. Foi criada uma função para converter
+um valor de dolar para reais.
+Essa função tem como parâmetro o valor em dolar. 
+Portanto, foi criada uma variável constante com o valor 
+da cotação do dólar, que será informado pelo usuário. 
+A função vai retornar (guardar) o valor do parâmetro em dolar multiplicada pela
+cotação informada pelo usuário.
+Fora da função foi criada uma nova variável
+nomeada de meu dinheiro onde foi informado
+o parâmetro (valor em dolar) de 100 dolares.
+Vai aparecer no console o valor de 100 dolares
+convertida em reais, esse valor será multiplicado
+pelo valor informado da cotação do dólar.
+*/
+
