41e06ef3c64475067bf0c77d200d0c68e6fa3f8d
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-06-03 17:22:40 -0300

    adicionei função de deletar usuário cadastrado

--- semana7/Labenusers/src/components/UsersListPage.js
+++ semana7/Labenusers/src/components/UsersListPage.js
@@ -1,6 +1,12 @@
 import React, { useReducer } from "react";
 import axios from "axios";
 
+const axiosAuthorization = {
+  headers: {
+    Authorization: "tatiana-fischer-mello",
+  },
+};
+
 class UsersListPage extends React.Component {
   //pegar cada item do banco de dados, utilizando
   //a documentação do back-end
@@ -8,13 +14,13 @@ class UsersListPage extends React.Component {
     usersList: [],
   };
 
-  fetchUsersList = () => {
-    const axiosAuthorization = {
-      headers: {
-        Authorization: "tatiana-fischer-mello",
-      },
-    };
+  componentDidMount() {
+    //toda vez que atualizar a página
+    this.fetchUsersList();
+  }
 
+  fetchUsersList = () => {
+    //PEGAR TODOS OS USUÁRIOS ATRAVÉS DO ID----------------
     axios
       .get(
         "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
@@ -24,14 +30,32 @@ class UsersListPage extends React.Component {
         this.setState({ usersList: response.data });
       });
   };
+  //DELETAR USUÁRIO ---------------------------------------------
+  userDeletion = (userId) => {
+    //para deletar seguir a documentação
+    axios
+      .delete(
+        //o link da documentação vai ser acrescentado do id do user mapeado na renderização
+        `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${userId}`,
+        axiosAuthorization
+      )
+      .then(() => {
+        alert(`O usuário foi deletado com sucesso `);
+        this.fetchUsersList();
+      })
+      .catch((error) => {
+        alert("ERRO AO APAGAR USUARIO");
+      });
+  };
+
   render() {
     return (
       <ul>
-        {this.state.usersList.mao((user) => {
+        {this.state.usersList.map((user) => {
           return (
             <li>
               {user.name}
-              <button onClick={() => this.UserDeletion(user.id)}>x</button>
+              <button onClick={() => this.userDeletion(user.id)}>x</button>
             </li>
           );
         })}
