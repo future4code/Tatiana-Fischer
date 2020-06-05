e9849037ea82351cad0b288f6827157197494d1a
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-06-05 16:07:39 -0300

    adicionei função de criar playlist

--- semana7/projeto-sexta-Labefy/labefy-projeto/src/App.js
+++ semana7/projeto-sexta-Labefy/labefy-projeto/src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+import axios from "axios";
+
+export default class App extends React.Component {
+  state = {
+    playlists: [],
+    newPlaylists: "",
+  };
+  inputPlaylistName = (event) => {
+    this.setState({ newPlaylists: event.target.value });
+    //console.log(this.state.newPlaylists);
+  };
+
+  componentDidMount = () => {
+    this.listPlaylists();
+  };
+
+  listPlaylists = () => {
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists",
+        {
+          headers: {
+            Athotization: "tatiana-fischer-mello",
+          },
+        }
+      )
+      .then((response) => {
+        this.setState({ playlists: response.data.result.list });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  createPlaylist = () => {
+    const body = {
+      name: this.state.newPlaylists,
+    };
+    axios
+      .post(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists",
+        body,
+        {
+          headers: {
+            Athotization: "tatiana-fischer-mello",
+          },
+        }
+      )
+      .then((response) => {
+        alert("Nova playlist criada com sucesso!");
+        console.log(response.data);
+        //this.setState({ newPlaylists: "" });
+        //this.listPlaylists()
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Ocorreu um erro");
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>New Playlist</h3>
+        <label>Name:</label>
+        <input
+          type="text"
+          value={this.state.newPlaylists.name}
+          onChange={this.inputPlaylistName}
+        ></input>
+        <button onClick={this.createPlaylist}>Save</button>
+      </div>
+    );
+  }
 }
-
-export default App;
