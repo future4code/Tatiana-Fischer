d30fced3a00b50c3211b24735f9fe61fe3b1d67f
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-06-05 11:11:53 -0300

    deletei uma pasta errada

---
+++ semana7/API-SpaceX/api-space-x/build/precache-manifest.1cacb8ee64724feb26310db64bc383ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b89705b3ff1d3141c53f95c5cc77ef37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "107267ec5c8eaacaafd1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4cc32e21c6f2e183c545",
+    "url": "/static/js/2.4a0bde3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a0bde3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107267ec5c8eaacaafd1",
+    "url": "/static/js/main.c5dd49b9.chunk.js"
+  },
+  {
+    "revision": "69f8396010f19514b6e0",
+    "url": "/static/js/runtime-main.97c939c8.js"
+  }
+]);
