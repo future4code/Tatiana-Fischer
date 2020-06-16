977eb7292188447165f86cd4e5456df2c736d3e1
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-06-16 08:36:11 -0300

    arrumei exercicio

---
+++ semana7/projeto-sexta-Labefy/labefy-projeto/build/precache-manifest.25747528bf021027a412d93f0c636e9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ca7bed8385f6de6d0edebf66b1ab6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "883a917afc3f9678b7c6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "271669d0545bfb8bcbc5",
+    "url": "/static/js/2.a06fe45a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a06fe45a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "883a917afc3f9678b7c6",
+    "url": "/static/js/main.7ec884bd.chunk.js"
+  },
+  {
+    "revision": "b5143c2f85483245a607",
+    "url": "/static/js/runtime-main.62a65cd9.js"
+  }
+]);
