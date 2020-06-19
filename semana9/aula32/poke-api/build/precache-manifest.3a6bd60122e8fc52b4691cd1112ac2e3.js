36a37563ff0135317869c97a5ee1941389292ed3
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-06-19 16:53:33 -0300

    mudando de branch

---
+++ semana9/aula32/poke-api/build/precache-manifest.3a6bd60122e8fc52b4691cd1112ac2e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27752881b001368842d5b1c866c06efc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c28f810fa96a26ff3613",
+    "url": "/static/css/main.53376f60.chunk.css"
+  },
+  {
+    "revision": "27ef06cb8c1c5f6ecdb4",
+    "url": "/static/js/2.1947c074.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1947c074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c28f810fa96a26ff3613",
+    "url": "/static/js/main.c427cbe1.chunk.js"
+  },
+  {
+    "revision": "9ebbe85cdae0b3cdaf22",
+    "url": "/static/js/runtime-main.54bd0e7c.js"
+  }
+]);
