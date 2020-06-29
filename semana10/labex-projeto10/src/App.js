24a95e5e3d80a8625057ee5948739c43eb099748
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-06-29 08:43:00 -0300

    adicionei CSS da pagina de detalhes

--- semana10/labex-projeto10/src/App.js
+++ semana10/labex-projeto10/src/App.js
@@ -7,9 +7,7 @@ import "./App.css";
 import styled from "styled-components";
 const ContainerApp = styled.div`
   background-image: url("https://cdn.pixabay.com/photo/2016/11/21/12/39/cosmos-1845140_1280.jpg");
-  /* https://cdn.pixabay.com/photo/2015/03/26/18/36/satellite-693226_1280.jpg */
-  /* https://images.unsplash.com/photo-1544942579-9671c890fe89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=886&q=80 */
-  /* background-image: url("https://images.unsplash.com/photo-1541186877-bb5a745edde5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80"); */
+
   background-repeat: space;
   background-attachment: fixed;
   background-position: center;
@@ -17,6 +15,7 @@ const ContainerApp = styled.div`
   background-size: cover;
   /* opacity: 0.8; */
   color: white;
+  min-height: 800px;
 `;
 
 function App() {
--- semana10/labex-projeto10/src/components/TripDetailsPage/TripDetailsPage.js
+++ semana10/labex-projeto10/src/components/TripDetailsPage/TripDetailsPage.js
@@ -2,6 +2,25 @@ import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import AdmMenu from "../AdmMenu/AdmMenu";
 import axios from "axios";
+import styled from "styled-components";
+
+const DetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: black;
+  align-items: flex-start;
+  width: 400px;
+  padding: 80px;
+  border-radius: 8px;
+  opacity: 0.8;
+`;
+
+const DetailsPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+`;
 
 function TripDetailsPage() {
   const [tripDetail, setTripDetail] = useState({});
@@ -47,11 +66,39 @@ function TripDetailsPage() {
   const baseUrl =
     "https://us-central1-labenu-apis.cloudfunctions.net/labeX/tatiana";
 
+  const goToCandidates = () => {
+    history.push(`/candidates`);
+  };
+
   return (
-    <div>
+    <DetailsPageContainer>
       <AdmMenu />
-      <p>{tripDetail.name}</p>
-    </div>
+      <DetailsContainer>
+        <h5>
+          Nome: {""}
+          {tripDetail.name}
+        </h5>
+        <h5>
+          Planeta: {""}
+          {tripDetail.planet}
+        </h5>
+        <h5>
+          Data: {""}
+          {tripDetail.date}
+        </h5>
+
+        <h5>
+          Descrição: {""}
+          {tripDetail.description}
+        </h5>
+        <h5>
+          Duração em dias: {""}
+          {tripDetail.durationInDays}
+        </h5>
+
+        <button onClick={goToCandidates}>VER CANDIDATOS</button>
+      </DetailsContainer>
+    </DetailsPageContainer>
   );
 }
 
