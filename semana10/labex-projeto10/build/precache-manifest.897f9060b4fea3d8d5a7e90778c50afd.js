65bef1e0a6ce41aeefd413cc6f823585e72b17e5
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-07-01 14:15:32 -0300

    mergiei

---
+++ semana10/labex-projeto10/build/precache-manifest.897f9060b4fea3d8d5a7e90778c50afd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa6833dc56dd5b77daaa5b24f013d88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e51685195dc27bec906b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f355a4a51c764537cf7",
+    "url": "/static/js/2.26c5cd15.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.26c5cd15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e51685195dc27bec906b",
+    "url": "/static/js/main.36fb97ab.chunk.js"
+  },
+  {
+    "revision": "0ee890c80bbe7256d14b",
+    "url": "/static/js/runtime-main.bd11ff85.js"
+  }
+]);
