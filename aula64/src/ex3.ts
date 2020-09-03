896a9a0c02346fc3ba49bec7a45e2dee693384af
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-09-03 15:22:29 -0300

    exercicio 3 a

---
+++ aula64/src/ex3.ts
@@ -0,0 +1,44 @@
+import { Character, validateCharacter } from "./ex1";
+
+// export const performAttack = (attacker: Character, defender: Character) => {
+//   if (!validateCharacter(attacker) || !validateCharacter(defender)) {
+//     throw new Error("Invalid character");
+//   }
+
+//   if (attacker.attack > defender.defense) {
+//     const oldLife = defender.life;
+//     const atackMenosDefesa = attacker.attack - defender.defense;
+//     const newLife = oldLife - atackMenosDefesa;
+//     console.log("nova vida:", newLife);
+//   } else {
+//     const newDefenseOfDefender = (defender.defense -= attacker.attack);
+//     console.log(
+//       "o defensor não perdeu vida, mas sua nova força diminuiu para",
+//       newDefenseOfDefender
+//     );
+//   }
+// };
+
+//b. inversão de depêndencia:
+export const performAttack = (
+  attacker: Character,
+  defender: Character,
+  validator: (input: Character) => boolean
+) => {
+  if (!validator(attacker) || !validator(defender)) {
+    throw new Error("Invalid character");
+  }
+
+  if (attacker.attack > defender.defense) {
+    const oldLife = defender.life;
+    const atackMenosDefesa = attacker.attack - defender.defense;
+    const newLife = oldLife - atackMenosDefesa;
+    console.log("nova vida:", newLife);
+  } else {
+    const newDefenseOfDefender = (defender.defense -= attacker.attack);
+    console.log(
+      "o defensor não perdeu vida, mas sua nova força diminuiu para",
+      newDefenseOfDefender
+    );
+  }
+};
--- aula64/src/index.ts
+++ aula64/src/index.ts
@@ -1,10 +1,27 @@
 import { Character, validateCharacter } from "./ex1";
+import { performAttack } from "./ex3";
 
-const sheep: Character = {
-  name: "ovelha da sorte",
-  life: 0,
-  defense: 1000,
-  attack: 50,
+// const sheep: Character = {
+//   name: "ovelha da sorte",
+//   life: 0,
+//   defense: 1000,
+//   attack: 50,
+// };
+
+// console.log(validateCharacter(sheep));
+
+const atacando: Character = {
+  attack: 60,
+  defense: 100,
+  life: 1000,
+  name: "atacador",
 };
 
-console.log(validateCharacter(sheep));
+const defendendo: Character = {
+  attack: 100,
+  defense: 100,
+  life: 100,
+  name: "defesa",
+};
+
+performAttack(atacando, defendendo);
