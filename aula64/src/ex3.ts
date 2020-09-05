32fc417c28e5f610d71bba0102810175db703f18
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-09-05 11:39:39 -0300

    ex 4

--- aula64/src/ex3.ts
+++ aula64/src/ex3.ts
@@ -1,31 +1,7 @@
 import { Character, validateCharacter } from "./ex1";
 
-// export const performAttack = (attacker: Character, defender: Character) => {
-//   if (!validateCharacter(attacker) || !validateCharacter(defender)) {
-//     throw new Error("Invalid character");
-//   }
-
-//   if (attacker.attack > defender.defense) {
-//     const oldLife = defender.life;
-//     const atackMenosDefesa = attacker.attack - defender.defense;
-//     const newLife = oldLife - atackMenosDefesa;
-//     console.log("nova vida:", newLife);
-//   } else {
-//     const newDefenseOfDefender = (defender.defense -= attacker.attack);
-//     console.log(
-//       "o defensor não perdeu vida, mas sua nova força diminuiu para",
-//       newDefenseOfDefender
-//     );
-//   }
-// };
-
-//b. inversão de depêndencia:
-export const performAttack = (
-  attacker: Character,
-  defender: Character,
-  validator: (input: Character) => boolean
-) => {
-  if (!validator(attacker) || !validator(defender)) {
+export const performAttack = (attacker: Character, defender: Character) => {
+  if (!validateCharacter(attacker) || !validateCharacter(defender)) {
     throw new Error("Invalid character");
   }
 
@@ -42,3 +18,27 @@ export const performAttack = (
     );
   }
 };
+
+// //b. inversão de depêndencia:
+// export const performAttack = (
+//   attacker: Character,
+//   defender: Character,
+//   validator: (input: Character) => boolean
+// ) => {
+//   if (!validator(attacker) || !validator(defender)) {
+//     throw new Error("Invalid character");
+//   }
+
+//   if (attacker.attack > defender.defense) {
+//     const oldLife = defender.life;
+//     const atackMenosDefesa = attacker.attack - defender.defense;
+//     const newLife = oldLife - atackMenosDefesa;
+//     console.log("nova vida:", newLife);
+//   } else {
+//     const newDefenseOfDefender = (defender.defense -= attacker.attack);
+//     console.log(
+//       "o defensor não perdeu vida, mas sua nova força diminuiu para",
+//       newDefenseOfDefender
+//     );
+//   }
+// };
