4176f2376cf79824f331684705905f5d11cce275
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-09-29 16:13:19 -0300

    exercícios lógica dia 2

---
+++ desafio-logica/exercicio.js
@@ -0,0 +1,9 @@
+function isNature(number) {
+  if ((Math.trunc(number) = number)) {
+    console.log("é natural");
+  }
+  console.log("não é natural");
+}
+
+console.log(isNature(5));
+console.log("oi");
